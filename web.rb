761795badb3bdec8e16a79f643330e595d014cec
Christian Martinez <chrismtz100@gmail.com>
2018-03-25 21:41:05 -0500

    Initial commit

---
+++ web.rb
@@ -0,0 +1,16 @@
+require 'sinatra'
+require 'net/http'
+require 'json'
+require 'openssl'
+
+OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+
+def get_search_count(term)
+# your code here
+end
+
+
+get '/' do
+# your code here
+end
+
